52d9d762a35265b77b32c21a448c1130437ef693
Dilshodbek078 <d.orazbayev@newuu.uz>
2025-11-14 23:51:37 +0500

    Update problem4.cpp

--- problem4.cpp
+++ problem4.cpp
@@ -3,20 +3,30 @@
 using namespace std;
 
 int main() {
-        vector<int> v;
-        int n=0, even=0, odd = 0;
-       while (n>=0) {
-           cin>>n;
-           v.push_back(n);
-           if (n%2==0)
-               even++;
-           else if (n%2==1)
-               odd++;
-       }
-    cout << "Even: " << even << endl;
-    cout << "Odd: " << odd << endl;
+    vector<int> v;
+    int n, x, unique=0;
+    cin>>n;
+    for (int i=0; i<n;i++){
+        cin >> x;
+        v.push_back(x);
+    }
+    for (int i=0; i<n;i++) {
+        bool is_unique = true;
+        for (int j=0; j<i;j++) {
+            cout << v.at(i) << " == " << v.at(j) << endl;
+            if (v.at(i)==v.at(j)) {
+                is_unique=false;
+                break;
+            }
+        }
+        if (is_unique) {
+            unique++;
+            cout << endl << "Unique++ = " << unique << endl;
+        }
+    }
+
+    cout << "Unique: " << unique << endl;
 
 
-
-        return 0;
+    return 0;
 }
