a302f4eb8aec2f3bff6f0e981e5a9983f251314b
Dilshodbek078 <d.orazbayev@newuu.uz>
2025-11-14 23:48:46 +0500

    Update problem5.cpp

--- problem5.cpp
+++ problem5.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main() {
+    int n;
+    cout<<"Enter Number of Elements: ";
+    cin >> n;
+
+    vector<int> a(n), b(n), c(n);
+
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        c[i] = a[i] + b[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << c[i] << " ";
+    }
+
+    return 0;
+}
